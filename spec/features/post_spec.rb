d596ef9023d5ea56d2c21e561cd0ec42e25065b7
p-duke <peteraaronduke@gmail.com>
2016-10-21 17:03:41 -0400

    updated tests for homepage

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,6 +11,13 @@ describe 'navigate' do
       visit posts_path
       expect(page).to have_content(/Posts/)
     end
+
+    it "had a list of posts" do
+      post1 = Post.create(date: Date.today, rationale: "Post1")
+      post2 = Post.create(date: Date.today, rationale: "Post2")
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
+    end
   end
 
     describe "creation" do
