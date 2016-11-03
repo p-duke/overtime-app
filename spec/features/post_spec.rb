eb5e5c501acd75dc2bb969252b78b0b832c61f7d
p-duke <peteraaronduke@gmail.com>
2016-11-02 20:40:39 -0400

    created base layout

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "Jon", last_name: "Snow")
+        login_as(user, :scope => :user)
+  end
+
   describe 'index' do
-    it "can be reached successfully" do
+    before do
       visit posts_path
+    end
+    it "can be reached successfully" do
       expect(page.status_code).to eq(200)
     end
 
     it "has a title of Posts" do
-      visit posts_path
       expect(page).to have_content(/Posts/)
     end
 
@@ -22,8 +28,7 @@ describe 'navigate' do
 
     describe "creation" do
       before do
-        user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "Jon", last_name: "Snow")
-        login_as(user, :scope => :user)
+
         visit new_post_path
       end
 
