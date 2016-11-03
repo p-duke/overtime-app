6669b36be2ae5cd5937bb15bbb4041f83a34865a
p-duke <peteraaronduke@gmail.com>
2016-11-03 17:46:37 -0400

    implemented the ability to delete post records

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,30 +7,40 @@ describe 'navigate' do
   end
 
   describe 'index' do
-    before do
-      visit posts_path
-    end
-    it "can be reached successfully" do
-      expect(page.status_code).to eq(200)
-    end
+      before do
+        visit posts_path
+      end
+      it "can be reached successfully" do
+        expect(page.status_code).to eq(200)
+      end
 
-    it "has a title of Posts" do
-      expect(page).to have_content(/Posts/)
-    end
+      it "has a title of Posts" do
+        expect(page).to have_content(/Posts/)
+      end
 
-    it "had a list of posts" do
-      post1 = FactoryGirl.build_stubbed(:post)
-      post2 = FactoryGirl.build_stubbed(:second_post)
-      visit posts_path
-      expect(page).to have_content(/Rationale|content/)
+      it "had a list of posts" do
+        post1 = FactoryGirl.build_stubbed(:post)
+        post2 = FactoryGirl.build_stubbed(:second_post)
+        visit posts_path
+        expect(page).to have_content(/Rationale|content/)
+      end
     end
-  end
 
   describe 'new' do
     it 'has a link from the homepage' do
       visit root_path
 
       click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
+  describe 'delete' do
+    it 'can be deleted' do
+      @post = FactoryGirl.create(:post)
+      visit posts_path
+
+      click_link("delete_post_#{@post.id}_from_index")
       expect(page.status_code).to eq(200)
     end
   end
@@ -53,6 +63,7 @@ describe 'navigate' do
 
       expect(page).to have_content("Some rationale")
     end
+
     it 'will have a user associated with it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "User Association"
