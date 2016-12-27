7a5261bc60261492c4f922f325a1de759bd12335
p-duke <peteraaronduke@gmail.com>
2016-12-27 15:08:45 -0500

    implemented ability to block users after post marked complete

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -28,5 +28,17 @@ describe 'navigate' do
 
       expect(page).to_not have_content('Approved')
     end
+
+    it 'should not be editable by the [post create if status is apprvoed' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      @post.update(user_id: user.id, status: 'approved')
+
+      visit edit_post_path(@post)
+
+      expect(current_path).to eq(root_path)
+    end
   end
 end
