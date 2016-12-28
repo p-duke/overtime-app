459a2f4ab71f6ed5388557b8dace10b33f33ad73
p-duke <peteraaronduke@gmail.com>
2016-12-27 19:41:37 -0500

    Added hours requested to post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 puts "1 User created"
 
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created!"
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,7 +4,7 @@ describe 'navigate' do
 let(:user) {FactoryGirl.create(:user)}
 
 let(:post) do
-  Post.create(date: Date.today, rationale: "Rationale", user_id: user.id)
+  Post.create(date: Date.today, rationale: "Rationale", user_id: user.id, overtime_request: 3.5)
 end
 
   before do
@@ -35,7 +35,7 @@ end
 
         other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: 'nonauth@example.com', password: 'password', password_confirmation: 'password')
 
-        post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
+        post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id, overtime_request: 3.5)
 
         visit posts_path
 
@@ -59,7 +59,7 @@ end
       delete_user = FactoryGirl.create(:user)
       login_as(delete_user, :scope => :user)
 
-      post_to_delete = Post.create(date: Date.today, rationale: 'new rationale', user_id: delete_user.id)
+      post_to_delete = Post.create(date: Date.today, rationale: 'new rationale', user_id: delete_user.id, overtime_request: 3.5)
 
       visit posts_path
 
@@ -78,18 +78,17 @@ end
     end
 
     it 'can be created from new form page' do
-
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
+      fill_in 'post[overtime_request]', with: 4.5
 
-      click_on "Save"
-
-      expect(page).to have_content("Some rationale")
+      expect {click_on "Save"}.to change(Post, :count).by(1)
     end
 
     it 'will have a user associated with it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "User Association"
+      fill_in 'post[overtime_request]', with: 4.5
       click_on "Save"
 
       expect(User.last.posts.last.rationale).to eq("User Association")
